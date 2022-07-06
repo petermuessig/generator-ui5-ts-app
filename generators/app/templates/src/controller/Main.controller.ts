c68fea1ee683e57f2fa9d6127f62d721828cac2e
Peter Muessig <peter.muessig@sap.com>
2022-07-06 19:27:11 +0200

    chore: some cleanup

--- generators/app/templates/src/controller/Main.controller.ts
+++ generators/app/templates/src/controller/Main.controller.ts
@@ -1,13 +1,8 @@
-import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
 
 /**
  * @namespace <%= appId %>.controller
  */
 export default class Main extends BaseController {
-
-	public sayHello() : void {
-		MessageBox.show("Hello World!");
-	}
 
 }
